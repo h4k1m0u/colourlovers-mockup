d539fe3bb604435c1def2e1a68e9903d97e984c8
h4k1m <h.benoudjit@gmail.com>
2014-03-21 17:19:37 +0100

    - prepare app for deployment on httpd & mod_wsgi

--- colourlovers_mockup.py
+++ colourlovers_mockup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 from flask import Flask, render_template
 from colourlovers import ColourLovers
 import os
@@ -16,15 +15,30 @@ def home():
     palette_id, palette_title, colours = palette.id, palette.title, palette.colours
 
     return render_template('mockup.html', **{
-        'colour1': colours[0],
-        'colour2': colours[1],
-        'colour3': colours[2],
-        'colour4': colours[3],
-        'colour5': colours[4],
+        'colour1': get_colour(colours, 0),
+        'colour2': get_colour(colours, 1),
+        'colour3': get_colour(colours, 2),
+        'colour4': get_colour(colours, 3),
+        'colour5': get_colour(colours, 4),
         'palette_id': palette_id,
         'palette_title': palette_title,
-        'getmtime': os.path.getmtime
+        'mtime': str(os.path.getmtime(app.root_path + '/static/style.css'))
     })
+
+def get_colour(l, i):
+    """ Returns colour of index 'i' from list 'l' if it exists, else return #00000
+      Args:          
+          l (list): list of colours
+          i (int): index of the colour to get
+      Returns:       
+          colour (int): colour returned
+    """
+    try:
+        colour = l[i]
+    except IndexError:
+        colour = '#000000'
+
+    return colour
 
 # run app
 if __name__ == '__main__':
